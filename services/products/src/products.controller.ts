b03eaf4db4161b38409c5f8fe827bdebe008aa9c
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-04-30 19:22:06 +0530

    product services testing

--- services/products/src/products.controller.ts
+++ services/products/src/products.controller.ts
@@ -13,7 +13,7 @@ import { UpdateProductDto } from './products/dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
@@ -32,7 +32,11 @@ export class ProductsController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+    try {
+      return this.productsService.update(id, updateProductDto);
+    } catch (error) {
+      console.log('Product ****************************', error);
+    }
   }
 
   @Delete(':id')
