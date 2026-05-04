9446b8db31a3fc7534aa74a17a554bc3e251f637
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-05-04 21:41:46 +0530

    docker compose file created

--- services/products/src/main.ts
+++ services/products/src/main.ts
@@ -10,17 +10,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const { appName, appPort } = getAppConfig();
+  console.log('Hello file changes.........');
   // const { nodeEnv } = getAppCommonConfig();
   // const log = new Logger(appName);
 
   const app = await NestFactory.create(ProductsModule, { bufferLogs: true });
+  console.log('1. Starting the app.........');
+
   //adding validation to the app
   //allow only whitelisted properties and forbid non-whitelisted properties
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   setupSwagger(app, appName, ['/auth-service']);
+  console.log('2. Starting the app.........');
   await app.init();
   await app.listen(appPort);
+  console.log('3. Starting the app.........');
+
   // logBootstrapInfo(app);
 }
 bootstrap();
