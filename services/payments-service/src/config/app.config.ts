796cf9ec469d9567e84ed5155ac8e8eb2342395f
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-05-11 18:47:46 +0530

    docker file and compose and env files updated

--- services/payments-service/src/config/app.config.ts
+++ services/payments-service/src/config/app.config.ts
@@ -6,7 +6,7 @@ import { registerAs } from '@nestjs/config';
 
 export const getAppConfig = () => ({
   appName: process.env.PAYMENTS_SERVICE_APP_NAME || 'Payments Service',
-  httpPort: process.env.PAYMENTS_SERVICE_APP_PORT || 5004,
+  httpPort: process.env.PAYMENTS_SERVICE_APP_PORT || 5003,
   microserviceName: MicroserviceName.PaymentsService,
   authHost: process.env.AUTH_HOST,
   authTcpPort: process.env.AUTH_TCP_PORT,
