2db360234aa3d0a307ee1dfd640a327034d86232
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-05-18 13:06:02 +0530

    stripe payment updated

--- services/payments-service/src/payments.service.ts
+++ services/payments-service/src/payments.service.ts
@@ -3,7 +3,7 @@
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { getAppConfig } from './config';
-import { CreateChargeDto } from '@libs/common';
+// import { CreateChargeDto } from '@libs/common';
 
 @Injectable()
 export class PaymentsService {
@@ -12,7 +12,7 @@ export class PaymentsService {
   // eslint-disable-next-line @typescript-eslint/require-await
   async createCharge(data): Promise<any> {
     const customer = await this.stripe.customers.create({
-      name: 'Jenny Rosen',
+      name: 'Jenny Rosen', //change this to dynamic
       email: data.user.email,
     });
     console.log('Customer', customer);
@@ -23,18 +23,49 @@ export class PaymentsService {
       },
     );
     console.log('customerSource', customerSource);
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card: customerSource
-    });
+
+    // const cardToken = {
+    //   id: 'card_1MvoiELkdIwHu7ixOeFGbN9D',
+    //   object: 'card',
+    //   address_city: null,
+    //   address_country: null,
+    //   address_line1: null,
+    //   address_line1_check: null,
+    //   address_line2: null,
+    //   address_state: null,
+    //   address_zip: null,
+    //   address_zip_check: null,
+    //   brand: 'Visa',
+    //   country: 'US',
+    //   customer: 'cus_NhD8HD2bY8dP3V',
+    //   cvc_check: null,
+    //   dynamic_last4: null,
+    //   exp_month: 4,
+    //   exp_year: 2024,
+    //   fingerprint: 'mToisGZ01V71BCos',
+    //   funding: 'credit',
+    //   last4: '4242',
+    //   metadata: {},
+    //   name: null,
+    //   tokenization_method: null,
+    //   wallet: null,
+    // };
+    // const paymentMethod = await this.stripe.paymentMethods.create({
+    //   type: 'card',
+    //   card: cardToken,
+    // });
+    // console.log('payment method', paymentMethod);
+
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
-      amount: data.charge.amount * 100,
-      confirm: true,
+      amount: 500,
+      currency: 'gbp',
+      payment_method: 'pm_card_in',
       payment_method_types: ['card'],
-      currency: 'usd',
+      customer: customer.id,
+      receipt_email: data.user.email,
     });
+
     console.log('Payments intent-', paymentIntent);
-    // return paymentIntent;
+    return paymentIntent;
   }
 }
