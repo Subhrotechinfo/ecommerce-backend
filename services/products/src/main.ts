0e7a70f10228277f64e01fda66f1d9a470bcf32a
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-05-29 17:00:12 +0530

    fixes

--- services/products/src/main.ts
+++ services/products/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   //adding validation to the app
-  //allow only whitelisted properties and forbid non-whitelisted propertiess
+  //allow only whitelisted properties and forbid non-whitelisted propertiesss
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   setupSwagger(app, appName, ['/products-service']);
